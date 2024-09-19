e8b6ba660fdff3881ce83cde8b2a8f2f4f5c43f1
DarthPedro <darthpedro@gmail.com>
2024-09-19 14:55:56 -0700

    Clean up repository constants.

--- src/BudgetTracker/Persistence/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿using D20Tek.Minimal.Functional;
-
-namespace BudgetTracker.Persistence;
-
-internal static class Constants
-{
-    public const string DatabaseFile = "budget-data.json";
-    public const string AppFolder = "\\d20tek-fin";
-}
--- src/BudgetTracker/Persistence/RepositoryFactory.cs
+++ src/BudgetTracker/Persistence/RepositoryFactory.cs
@@ -6,11 +6,14 @@ namespace BudgetTracker.Persistence;
 
 internal static class RepositoryFactory
 {
+    private const string _databaseFile = "budget-data.json";
+    private const string _appFolder = "\\d20tek-fin";
+
     private static readonly LowDb<BudgetDataStore> _dataStore =
         LowDbFactory.CreateLowDb<BudgetDataStore>(b =>
-            b.UseFileDatabase(Constants.DatabaseFile)
+            b.UseFileDatabase(_databaseFile)
                 .WithFolder(Environment.GetFolderPath(
-                    Environment.SpecialFolder.ApplicationData) + Constants.AppFolder));
+                    Environment.SpecialFolder.ApplicationData) + _appFolder));
 
     public static ICategoryRepository CreateCategoryRepository() =>
         new FileRepository<BudgetCategory, BudgetDataStore>(_dataStore, store => store.Categories);
