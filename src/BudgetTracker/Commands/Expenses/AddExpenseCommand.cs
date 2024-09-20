137f8c1201004fcd6e75c6ea81b8ad2d5e89f8a8
Pedro Silva <darthpedro@gmail.com>
2024-09-19 23:03:10 -0700

    Completed add expense command. Verifies category Id exists in the database. Default committed date to current date. Valid the actual amount isn't negative.

--- src/BudgetTracker/Commands/Expenses/AddExpenseCommand.cs
+++ src/BudgetTracker/Commands/Expenses/AddExpenseCommand.cs
@@ -9,23 +9,34 @@ internal static class AddExpenseCommand
 {
     public static AppState Handle(AppState state, CommandTypeMetadata metadata) =>
         state.Apply(s => s.Console.DisplayHeader(Constants.Add.Header))
-             .Map(s => s.Console.GatherExpenseData()
+             .Map(s => s.Console.GatherExpenseData(s.CategoryRepo)
                 .Map(entry => s.ExpenseRepo.Create(entry))
                 .Apply(result => s.Console.DisplayMaybe(result, Constants.Add.SuccessMessage))
                 .Map(_ => s with { Command = metadata.Name }));
 
-    private static Expense GatherExpenseData(this IAnsiConsole console) =>
-        new(0, console.GetName(), console.GetCategoryId(), console.GetCommittedDate(), console.GetActual());
+    private static Expense GatherExpenseData(this IAnsiConsole console, ICategoryRepository catRepo) =>
+        new(0, console.GetName(), console.GetCategoryId(catRepo), console.GetCommittedDate(), console.GetActual());
 
     private static string GetName(this IAnsiConsole console) =>
         console.Prompt(new TextPrompt<string>(Constants.Add.NameLabel));
 
-    private static int GetCategoryId(this IAnsiConsole console) =>
-        console.Prompt(new TextPrompt<int>(Constants.Add.CategoryLabel));
+    private static int GetCategoryId(this IAnsiConsole console, ICategoryRepository catRepo) =>
+        console.Prompt(new TextPrompt<int>(Constants.Add.CategoryLabel)
+                            .Validate(
+                                x => catRepo.GetEntityById(x) is Something<BudgetCategory>,
+                                Constants.Add.InvalidCatIdError));
 
     private static DateTimeOffset GetCommittedDate(this IAnsiConsole console) =>
-        console.Prompt(new TextPrompt<DateTimeOffset>(Constants.Add.DateLabel));
+        console.Prompt(new TextPrompt<DateTimeOffset>(Constants.Add.DateLabel)
+                            .DefaultValue(DateTimeOffset.Now)
+                            .WithConverter(date => date.ToDateString()));
 
     private static decimal GetActual(this IAnsiConsole console) =>
-        MoneyComponent.Input(console, Constants.Add.ActualLabel);
+        Constants.Add.InvalidActual
+            .IterateUntil(
+                _ => MoneyComponent.Input(console, Constants.Add.ActualLabel)
+                        .Apply(x => (x < 0).IfTrueOrElse(
+                            () => console.WriteMessage(Constants.Add.InvalidActualError))),
+                x => x >= 0);
 }
+
--- src/BudgetTracker/Commands/Expenses/Constants.cs
+++ src/BudgetTracker/Commands/Expenses/Constants.cs
@@ -26,8 +26,11 @@ internal static class Constants
         public const string Header = "Add Expense Item";
         public const string NameLabel = "Enter the expense title:";
         public const string CategoryLabel = "Enter expense category id:";
-        public const string DateLabel = "Enter date of the expense:";
+        public const string DateLabel = "Enter date of the expense";
         public const string ActualLabel = "Enter the expense actual amount:";
+        public const string InvalidCatIdError = "[red]That category id does not exist.[/]";
+        public const decimal InvalidActual = -1M;
+        public const string InvalidActualError = "[red]The expense actual cannot be negative.[/]";
 
         public static string[] SuccessMessage(Expense expense) =>
         [
