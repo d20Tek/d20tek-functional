58959a62950aafd066ec0883cb45333c0ce0d49e
Pedro Silva <darthpedro@gmail.com>
2024-09-20 22:20:20 -0700

    Implemented list credits command.

--- src/BudgetTracker/Commands/Configuration.cs
+++ src/BudgetTracker/Commands/Configuration.cs
@@ -1,4 +1,5 @@
 ﻿using BudgetTracker.Commands.BudgetCategories;
+using BudgetTracker.Commands.Credits;
 using BudgetTracker.Commands.Expenses;
 
 namespace BudgetTracker.Commands;
@@ -9,14 +10,18 @@ internal sealed class Configuration
     [
         new ("list-cat", ["list-categories", "list-cat", "lc"], ListCategoriesCommand.Handle),
         new ("add-cat", ["add-categories", "add-cat", "ac"], AddCategoryCommand.Handle),
-        new ("delete-cat", ["delete-category", "del-cat", "dc"], 
-            (s, m) => CommonHandlers.DeleteFromRepository(s, m, BudgetCategories.Constants.Delete.Header, id => s.CategoryRepo.Delete(id))),
         new ("edit-cat", ["edit-category", "edit-cat", "ec"], EditCategoryCommand.Handle),
+        new ("delete-cat", ["delete-category", "del-cat", "dc"],
+            (s, m) => CommonHandlers.DeleteFromRepository(s, m, BudgetCategories.Constants.Delete.Header, id => s.CategoryRepo.Delete(id))),
+
         new ("list-exp", ["list-expenses", "list-exp", "le"], ListExpenseCommand.Handle),
         new ("add-exp", ["add-expense", "add-exp", "ae"], AddExpenseCommand.Handle),
-        new ("del-exp", ["delete-expense", "del-exp", "de"], 
-            (s, m) => CommonHandlers.DeleteFromRepository(s, m, Expenses.Constants.Delete.Header, id => s.ExpenseRepo.Delete(id))),
         new ("edit-exp", ["edit-expense", "edit-exp", "ee"], EditExpenseCommand.Handle),
+        new ("del-exp", ["delete-expense", "del-exp", "de"],
+            (s, m) => CommonHandlers.DeleteFromRepository(s, m, Expenses.Constants.Delete.Header, id => s.ExpenseRepo.Delete(id))),
+
+        new ("list-cred", ["list-credits", "list-cred", "lcr"], ListCreditsCommand.Handle),
+
         new ("help", ["help", "h"], CommonHandlers.ShowCommands),
         new ("exit", ["exit", "x"], CommonHandlers.Exit)
     ];
---
+++ src/BudgetTracker/Commands/Credits/ListCreditsCommand.cs
@@ -0,0 +1,39 @@
+﻿using Apps.Common;
+using BudgetTracker.Entities;
+using D20Tek.Minimal.Functional;
+using Spectre.Console;
+
+namespace BudgetTracker.Commands.Credits;
+
+internal static class ListCreditsCommand
+{
+    public static AppState Handle(AppState state, CommandTypeMetadata metadata) =>
+        state.Apply(s => s.Console.WriteMessage(Constants.List.CreditsListHeader))
+             .Apply(s => s.Console.Write(CreateTable(s.CreditRepo.GetEntities())))
+             .Map(s => s with { Command = metadata.Name });
+
+    private static Table CreateTable(Credit[] credits) =>
+        new Table()
+            .Border(TableBorder.Rounded)
+            .AddColumns(
+                new TableColumn(Constants.List.ColumnId).Centered().Width(Constants.List.ColumnIdLen),
+                new TableColumn(Constants.List.ColumnName).Width(Constants.List.ColumnNameLen),
+                new TableColumn(Constants.List.ColumnDepositDate).Width(Constants.List.ColumnDepositDateLen),
+                new TableColumn(Constants.List.ColumnAmount).RightAligned().Width(Constants.List.ColumnAmountLen))
+            .Apply(t => t.AddRowsForEntries(credits));
+
+    private static void AddRowsForEntries(this Table table, Credit[] credits) =>
+        credits
+            .Map(e => (e.Length != 0)
+                ? credits.Select(entry => CreateRow(entry)).ToList()
+                : [[string.Empty, Constants.List.NoExpensesMessage, string.Empty, string.Empty]])
+            .Apply(rows => rows.ForEach(x => table.AddRow(x)));
+
+    private static string[] CreateRow(Credit credit) =>
+    [
+        credit.Id.ToString(),
+        credit.Name.CapOverflow(Constants.List.ColumnNameLen),
+        credit.DepositDate.ToDateString(),
+        CurrencyComponent.Render(credit.Amount)
+    ];
+}
