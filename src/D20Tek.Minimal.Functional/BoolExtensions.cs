bac81033e335b2228e4b18a198041ac0b3f698d6
DarthPedro <darthpedro@gmail.com>
2024-09-23 11:49:28 -0700

    Split out boolean extensions to separate file.

---
+++ src/D20Tek.Minimal.Functional/BoolExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace D20Tek.Minimal.Functional;
+
+public static partial class FunctionalExtensions
+{
+    public static TOut IfTrueOrElse<TOut>(this bool condition, Func<TOut> thenFunc, Func<TOut> elseFunc) =>
+        condition ? thenFunc() : elseFunc();
+
+    public static void IfTrueOrElse(this bool condition, Action thenAction, Action? elseAction = null)
+    {
+        if (condition)
+            thenAction();
+        else
+            elseAction?.Invoke();
+    }
+}
--- src/D20Tek.Minimal.Functional/FunctionalExtensions.cs
+++ src/D20Tek.Minimal.Functional/FunctionalExtensions.cs
@@ -36,17 +36,6 @@ public static partial class FunctionalExtensions
         }
     }
 
-    public static TOut IfTrueOrElse<TOut>(this bool condition, Func<TOut> thenFunc, Func<TOut> elseFunc) =>
-        condition ? thenFunc() : elseFunc();
-
-    public static void IfTrueOrElse(this bool condition, Action thenAction, Action? elseAction = null)
-    {
-        if (condition)
-            thenAction();
-        else
-            elseAction?.Invoke();
-    }
-
     public static T IterateUntil<T>(this T instance, Func<T, T> updateFunction, Func<T, bool> endCondition)
     {
         var currentThis = instance;
