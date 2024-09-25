58c72af06e812842e47f6fa8bd9934da3c6a50d5
DarthPedro <darthpedro@gmail.com>
2024-09-25 11:42:29 -0700

    Fixed GetSnapshotForMonth to return an error when snapshot not found.

--- src/BudgetTracker/Persistence/SnapshotRepositoryExtensions.cs
+++ src/BudgetTracker/Persistence/SnapshotRepositoryExtensions.cs
@@ -5,6 +5,11 @@ namespace BudgetTracker.Persistence;
 
 internal static class SnapshotRepositoryExtensions
 {
+    public static Maybe<ReconciledSnapshot> SnapshotNotFoundError =
+        new Failure<ReconciledSnapshot>(Error.Validation(
+            "Snapshot.NotFound",
+            "There was no reconciled snapshot for the month that you specified."));
+
     public static bool SnapshotExists(
         this IReconciledSnapshotRepository snapRepo,
         DateTimeOffset date) =>
@@ -13,5 +18,6 @@ internal static class SnapshotRepositoryExtensions
     public static Maybe<ReconciledSnapshot> GetSnapshotForMonth(
         this IReconciledSnapshotRepository snapRepo,
         DateTimeOffset date) =>
-        snapRepo.GetEntities().FirstOrDefault(x => x.StartDate == date).ToMaybeIfNull();
+        snapRepo.GetEntities().FirstOrDefault(x => x.StartDate == date)
+            .Map(snapshot => snapshot is null ? SnapshotNotFoundError : snapshot);
 }
