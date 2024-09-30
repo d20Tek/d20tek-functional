b514e01237dc862d55c5bc933193181cf2403f87
DarthPedro <darthpedro@gmail.com>
2024-09-30 15:39:02 -0700

    Implemented basic Choice class for two types. Added corresponding unit tests.

---
+++ src/D20Tek.Functional/Choice.cs
@@ -0,0 +1,54 @@
+﻿namespace D20Tek.Functional;
+
+public sealed class Choice<T1, T2>
+    where T1 : notnull
+    where T2 : notnull
+{
+    private readonly object _value;
+
+    public Choice(T1 value) => _value = value;
+
+    public Choice(T2 value) => _value = value;
+
+    public bool IsChoice1 => _value is T1;
+
+    public bool IsChoice2 => _value is T2;
+
+    public TResult Match<TResult>(Func<T1, TResult> func1, Func<T2, TResult> func2) =>
+        _value switch
+        {
+            T1 => func1((T1)_value),
+            T2 => func2((T2)_value),
+            _ => throw new ArgumentOutOfRangeException("Invalid Choice type")
+        };
+
+    public void Iter(Action<T1> action1, Action<T2> action2)
+    {
+        switch (_value)
+        {
+            case T1:
+                action1((T1)_value);
+                break;
+            case T2:
+                action2((T2)_value);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("Invalid Choice type");
+        }
+    }
+
+    public Choice<TResult, T2> Bind<TResult>(Func<T1, Choice<TResult, T2>> bindFunc) where TResult : notnull =>
+        Match(t1 => bindFunc(t1), t2 => new Choice<TResult, T2>(t2));
+
+    public T1 GetChoice1() => (T1)_value;
+
+    public T2 GetChoice2() => (T2)_value;
+
+    public Choice<TResult, T2> Map<TResult>(Func<T1, TResult> mapFunc) where TResult : notnull => 
+        Match(t1 => new Choice<TResult, T2>(mapFunc(t1)), t2 => new Choice<TResult, T2>(t2));
+
+    public override string ToString() =>
+        Match(
+            v1 => $"Choice<{typeof(T1).Name}>(value = {v1})",
+            v2 => $"Choice<{typeof(T2).Name}>(value = {v2})");
+}
---
+++ tests/D20Tek.Functional.UnitTests/ChoiceTests.cs
@@ -0,0 +1,173 @@
+﻿namespace D20Tek.Functional.UnitTests;
+
+[TestClass]
+public class ChoiceTests
+{
+    [TestMethod]
+    public void IsChoice1_WithValueType1_ReturnsTrue()
+    {
+        // arrange
+        var choice = new Choice<int, string>(42);
+
+        // act
+        var isChoice1 = choice.IsChoice1;
+        var isChoice2 = choice.IsChoice2;
+
+        // assert
+        isChoice1.Should().BeTrue();
+        isChoice2.Should().BeFalse();
+        choice.GetChoice1().Should().Be(42);
+        choice.ToString().Should().Be("Choice<Int32>(value = 42)");
+    }
+
+    [TestMethod]
+    public void IsChoice2_WithValueType2_ReturnsTrue()
+    {
+        // arrange
+        var choice = new Choice<int, string>("foo");
+
+        // act
+        var isChoice1 = choice.IsChoice1;
+        var isChoice2 = choice.IsChoice2;
+
+        // assert
+        isChoice1.Should().BeFalse();
+        isChoice2.Should().BeTrue();
+        choice.GetChoice2().Should().Be("foo");
+        choice.ToString().Should().Be("Choice<String>(value = foo)");
+    }
+
+    [TestMethod]
+    public void Bind_WithValidT1Value_ReturnsChoice1()
+    {
+        // arrange
+        var input = new Choice<string, int>("42");
+
+        // act
+        var result = input.Bind(v => TryParse(v));
+
+        // assert
+        result.IsChoice1.Should().BeTrue();
+        result.GetChoice1().Should().Be(42);
+    }
+
+    [TestMethod]
+    public void Bind_WithInvalidValue_ReturnsChoice2()
+    {
+        // arrange
+        var input = new Choice<string, int>("non-int-text");
+
+        // act
+        var result = input.Bind(v => TryParse(v));
+
+        // assert
+        result.IsChoice2.Should().BeTrue();
+        result.GetChoice2().Should().Be(0);
+    }
+
+    [TestMethod]
+    public void Bind_WithT2Value_ReturnsInitialChoice2()
+    {
+        // arrange
+        var input = new Choice<string, int>(101);
+
+        // act
+        var result = input.Bind([ExcludeFromCodeCoverage] (v) => TryParse(v));
+
+        // assert
+        result.IsChoice2.Should().BeTrue();
+        result.GetChoice2().Should().Be(101);
+    }
+
+    [TestMethod]
+    public void Map_WithChoice1Value_ReturnsChoice1()
+    {
+        // arrange
+        var input = new Choice<int, string>(42);
+
+        // act
+        var result = input.Map(v => v * 2);
+
+        // assert
+        result.IsChoice1.Should().BeTrue();
+        result.GetChoice1().Should().Be(84);
+    }
+
+    [TestMethod]
+    public void Map_WithChoice2Value_ReturnsChoice1()
+    {
+        // arrange
+        var input = new Choice<int, string>("foo");
+
+        // act
+        var result = input.Map([ExcludeFromCodeCoverage] (v) => v * 2);
+
+        // assert
+        result.IsChoice2.Should().BeTrue();
+        result.GetChoice2().Should().Be("foo");
+    }
+
+    [TestMethod]
+    [ExcludeFromCodeCoverage]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void Match_WithInvalidType_ThrowsException()
+    {
+        // arrange
+        var input = new Choice<int, string>(null!);
+
+        // act
+        var result = input.Map([ExcludeFromCodeCoverage] (v) => v * 2);
+
+        // assert
+    }
+
+    [TestMethod]
+    public void Iter_WithChoice1Value_CallsAction1()
+    {
+        // arrange
+        var output = string.Empty;
+        var input = new Choice<int, string>(42);
+
+        // act
+        input.Iter(v1 => output = "choice1 called", [ExcludeFromCodeCoverage] (v2) => output = "choice2 called");
+
+        // assert
+        output.Should().Be("choice1 called");
+    }
+
+    [TestMethod]
+    public void Iter_WithChoice2Value_CallsAction2()
+    {
+        // arrange
+        var output = string.Empty;
+        var input = new Choice<int, string>("foo");
+
+        // act
+        input.Iter([ExcludeFromCodeCoverage] (v1) => output = "choice1 called", v2 => output = "choice2 called");
+
+        // assert
+        output.Should().Be("choice2 called");
+    }
+
+    [TestMethod]
+    [ExcludeFromCodeCoverage]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void Iter_WithInvalidType_ThrowsException()
+    {
+        // arrange
+        var output = string.Empty;
+        var input = new Choice<int, string>(null!);
+
+        // act
+        input.Iter(
+            [ExcludeFromCodeCoverage] (v1) => output = "choice1 called",
+            [ExcludeFromCodeCoverage] (v2) => output = "choice2 called");
+
+        // assert
+    }
+
+    private static Choice<float, int> TryParse(string text) =>
+        float.TryParse(text, out float parsed)
+            ? new Choice<float, int>(parsed)
+            : new Choice<float, int>(0);
+}
