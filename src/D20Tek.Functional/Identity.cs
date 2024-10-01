67d7ebd9336c8db53825e5db7a8d37aed0f1dfdd
DarthPedro <darthpedro@gmail.com>
2024-10-01 15:39:51 -0700

    Completed implementation of Identity<T> class. Added all unit tests for that class.

--- src/D20Tek.Functional/Identity.cs
+++ src/D20Tek.Functional/Identity.cs
@@ -9,10 +9,21 @@ public sealed class Identity<T>
 
     public Identity<TResult> Bind<TResult>(Func<T, Identity<TResult>> bind) where TResult : notnull => bind(_value);
 
+    public bool Contains(T value) => _value.Equals(value);
+
+    public int Count() => 1;
+
+    public bool Exists(Func<T, bool> predicate) => predicate(_value);
+
     public T Get() => _value;
+
+    public void Iter(Action<T> action) => action(_value);
 
     public Identity<TResult> Map<TResult>(Func<T, TResult> mapper) where TResult : notnull => new(mapper(_value));
 
+    public override string ToString() => $"Identity<{typeof(T).Name}>(value = {_value})";
+
+    public static Identity<T> Create(T value) => new(value);
     public static implicit operator Identity<T>(T instance) => new(instance);
     public static implicit operator T(Identity<T> instance) => instance._value;
 }
---
+++ tests/D20Tek.Functional.UnitTests/IdentityTests.cs
@@ -0,0 +1,128 @@
+﻿namespace D20Tek.Functional.UnitTests;
+
+[TestClass]
+public class IdentityTests
+{
+    [TestMethod]
+    public void Get_WithCreatedValue_ReturnsValue()
+    {
+        // arrange
+        var v = Identity<int>.Create(13);
+
+        // act
+        var result = v.Get();
+
+        // assert
+        result.Should().Be(13);
+        v.ToString().Should().Be("Identity<Int32>(value = 13)");
+        v.Count().Should().Be(1);
+    }
+
+    [TestMethod]
+    public void Bind_WithValue_ReturnsIdentity()
+    {
+        // arrange
+        var id = Identity<string>.Create("42");
+
+        // act
+        var result = id.Bind(v => TryParse(v));
+
+        // assert
+        result.Get().Should().Be(42);
+    }
+
+    [TestMethod]
+    public void Bind_WithInvalidValue_ReturnsDefaultIdentity()
+    {
+        // arrange
+        var id = Identity<string>.Create("non-int-text");
+
+        // act
+        int result = id.Bind(v => TryParse(v));
+
+        // assert
+        result.Should().Be(0);
+    }
+
+    [TestMethod]
+    public void Contains_WithEqualValue_ReturnsTrue()
+    {
+        // arrange
+        var item = Identity<string>.Create("foo");
+
+        // act
+        bool contains = item.Contains("foo");
+
+        // assert
+        contains.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Contains_WithUnequalValue_ReturnsFalse()
+    {
+        // arrange
+        var item = Identity<string>.Create("foo");
+
+        // act
+        bool contains = item.Contains("bar");
+
+        // assert
+        contains.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Exists_WithPositivePredicate_ReturnsTrue()
+    {
+        // arrange
+        var item = Identity<int>.Create(42);
+
+        // act
+        var result = item.Exists(x => x > 10);
+
+        // assert
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Exists_WithNegativePredicate_ReturnsFalse()
+    {
+        // arrange
+        var item = Identity<int>.Create(42);
+
+        // act
+        var result = item.Exists(x => x == 10);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Iter_WithValue_PerformsAction()
+    {
+        // arrange
+        var output = string.Empty;
+        var option = Identity<string>.Create("Hello World");
+
+        // act
+        option.Iter(x => output = x);
+
+        // assert
+        output.Should().Be("Hello World");
+    }
+
+    [TestMethod]
+    public void Map_WithValue_ReturnsNewIdentity()
+    {
+        // arrange
+        var option = Identity<int>.Create(42);
+
+        // act
+        var result = option.Map(v => v * 2);
+
+        // assert
+        result.Get().Should().Be(84);
+    }
+
+    public static Identity<int> TryParse(string text) =>
+        int.TryParse(text, out int parsed) ? parsed : 0;
+}
