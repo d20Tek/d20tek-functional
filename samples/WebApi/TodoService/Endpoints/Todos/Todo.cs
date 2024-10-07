9c75528446220bf1c8fd0928381d24fe29ccac35
DarthPedro <darthpedro@gmail.com>
2024-10-07 16:16:28 -0700

    Refactored TodoEndpoint to more functional style.

--- samples/WebApi/TodoService/Endpoints/Todos/Todo.cs
+++ samples/WebApi/TodoService/Endpoints/Todos/Todo.cs
@@ -2,11 +2,25 @@
 
 public sealed class Todo
 {
-    public int Id { get; set; }
+    public int Id { get; private set; }
 
-    public string Title { get; set; } = string.Empty;
+    public string Title { get; private set; }
     
-    public string? Description { get; set; }
+    public string? Description { get; private set; }
     
-    public bool IsCompleted { get; set; }
+    public bool IsCompleted { get; private set; }
+
+    public Todo(int id, string title, string? description = null, bool isCompleted = false)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        IsCompleted = isCompleted;
+    }
+
+    public Todo SetId(int id)
+    {
+        Id = id;
+        return this;
+    }
 }
--- samples/WebApi/TodoService/Endpoints/Todos/TodoEndpoints.cs
+++ samples/WebApi/TodoService/Endpoints/Todos/TodoEndpoints.cs
@@ -1,45 +1,41 @@
-﻿namespace TodoService.Endpoints.Todos;
+﻿using D20Tek.Functional;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace TodoService.Endpoints.Todos;
 
 public static class TodoEndpoints
 {
-    public static void MapTodoEndpoints (this IEndpointRouteBuilder routes)
+    public static void MapTodoEndpoints(this IEndpointRouteBuilder routes) =>
+        routes.MapGroup("/api/Todo")
+              .WithTags(nameof(Todo))
+              .ToIdentity()
+              .Iter(g => g.MapGet("/", () => Get())
+                          .WithName("GetAllTodos")
+                          .WithOpenApi())
+              .Iter(g => g.MapGet("/{id}", (int id) => GetById(id))
+                          .WithName("GetTodoById")
+                          .WithOpenApi())
+              .Iter(g => g.MapPut("/{id}", (int id, Todo input) => Update(id, input))
+                          .WithName("UpdateTodo")
+                          .WithOpenApi())
+              .Iter(g => g.MapPost("/", (Todo model) => Create(model))
+                          .WithName("CreateTodo")
+                          .WithOpenApi())
+              .Iter(g => g.MapDelete("/{id}", (int id) => Delete(id))
+                          .WithName("DeleteTodo")
+                          .WithOpenApi());
+
+    private static Todo[] Get() => [new(1, "Test")];
+
+    private static Todo GetById(int id) => new(id, "Test");
+
+    private static Created<Todo> Create(Todo model)
     {
-        var group = routes.MapGroup("/api/Todo")
-                          .WithTags(nameof(Todo));
-
-        group.MapGet("/", () =>
-        {
-            return new [] { new Todo() };
-        })
-        .WithName("GetAllTodos")
-        .WithOpenApi();
-
-        group.MapGet("/{id}", (int id) =>
-        {
-            //return new Todo { ID = id };
-        })
-        .WithName("GetTodoById")
-        .WithOpenApi();
-
-        group.MapPut("/{id}", (int id, Todo input) =>
-        {
-            return TypedResults.NoContent();
-        })
-        .WithName("UpdateTodo")
-        .WithOpenApi();
-
-        group.MapPost("/", (Todo model) =>
-        {
-            //return TypedResults.Created($"/api/Todos/{model.ID}", model);
-        })
-        .WithName("CreateTodo")
-        .WithOpenApi();
-
-        group.MapDelete("/{id}", (int id) =>
-        {
-            //return TypedResults.Ok(new Todo { ID = id });
-        })
-        .WithName("DeleteTodo")
-        .WithOpenApi();
+        model.SetId(12);
+        return TypedResults.Created($"/api/Todos/{model.Id}", model);
     }
+
+    private static NoContent Update(int id, Todo input) => TypedResults.NoContent();
+
+    private static Ok<Todo> Delete(int id) => TypedResults.Ok(new Todo(id, "deleted"));
 }
