78bbc86b86998adfaf3f5cfa4c3a3bdbf8bd9743
DarthPedro <darthpedro@gmail.com>
2024-10-13 14:02:27 -0700

    Added MemberService project for api controller-based sample. Updated program.cs file to be functional style.

---
+++ samples/WebApi/MemberService/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using D20Tek.Functional;
+
+namespace MemberService;
+
+internal static class DependencyInjection
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder) =>
+        builder.Services.AddEndpointsApiExplorer()
+                        .AddSwaggerGen()
+                        .AddCors()
+                        .AddControllers()
+                        //.AddRepository()
+                        .Pipe(_ => builder);
+
+    public static WebApplication ConfigurePipeline(this WebApplication app) =>
+        app.ToIdentity()
+           .Iter(app => app.UseSwaggerInDev()
+                           .UseHttpsRedirection()
+                           .UseCors())
+           .Iter(app => app.MapControllers());
+
+    private static WebApplication UseSwaggerInDev(this WebApplication app) =>
+        app.Pipe(a => a.Environment.IsDevelopment()
+                       .IfTrueOrElse(
+                            () => a.UseSwagger()
+                                   .UseSwaggerUI()))
+           .Pipe(a => a);
+}
