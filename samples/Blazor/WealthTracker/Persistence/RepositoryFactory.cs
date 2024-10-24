7b2ce0a9853afd67d6ca146b367e9ece4378c688
Pedro Silva <DarthPedro@gmail.com>
2024-10-24 15:35:25 -0700

    changes to update the blazor sample app. likely temporary.

--- samples/Blazor/WealthTracker/Persistence/RepositoryFactory.cs
+++ samples/Blazor/WealthTracker/Persistence/RepositoryFactory.cs
@@ -1,18 +1,16 @@
 ﻿using Apps.Repositories;
 using D20Tek.LowDb;
+using D20Tek.LowDb.Browser;
 using WealthTracker.Domain;
 
 namespace WealthTracker.Persistence;
 
 internal static class RepositoryFactory
 {
-    public const string _databaseFile = "wealth-data.json";
-    public const string _appFolder = "\\d20tek-fin";
+    public const string _databaseKey = "wealth-data-key";
 
     public static IServiceCollection AddRepository(this IServiceCollection services) =>
-        services.AddLowDb<WealthDataStore>(b =>
-                    b.UseFileDatabase(_databaseFile)
-                     .WithFolder(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + _appFolder))
-                .AddSingleton<IWealthRepository, FileRepository<WealthDataEntity, WealthDataStore>>(sp =>
+        services.AddLocalLowDb<WealthDataStore>(_databaseKey)
+                .AddScoped<IWealthRepository, FileRepository<WealthDataEntity, WealthDataStore>>(sp =>
                     new(sp.GetRequiredService<LowDb<WealthDataStore>>(), store => store));
 }
