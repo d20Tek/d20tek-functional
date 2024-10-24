e5629a4cdf553cdb36348fa54a463255eaa7bcc0
Pedro Silva <DarthPedro@gmail.com>
2024-10-24 16:00:13 -0700

    removed old wealthtracker sample project.

--- samples/Blazor/WealthTracker-Old/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WealthTracker;
-
-internal static class Constants
-{
-    public static Exception FutureDateError(string propertyName) =>
-        new ArgumentOutOfRangeException(propertyName, "Date value for updates cannot be in the future.");
-}
