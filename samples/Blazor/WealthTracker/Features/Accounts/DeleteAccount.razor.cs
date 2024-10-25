e1de7676a9be4d5384e5ec10ace1f0902fc9b71e
Pedro Silva <DarthPedro@gmail.com>
2024-10-25 14:17:48 -0700

    Moved DeleteAccount logic to code behind file. Added some introductory text to the homepage.

---
+++ samples/Blazor/WealthTracker/Features/Accounts/DeleteAccount.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using WealthTracker.Domain;
+
+namespace WealthTracker.Features.Accounts;
+
+public partial class DeleteAccount
+{
+    private string _errorMessage = string.Empty;
+    private WealthDataEntity? _account;
+
+    [Parameter]
+    public int Id { get; set; }
+
+    protected override void OnInitialized()
+    {
+        var result = _repo.GetEntityById(Id);
+        result.Match(
+            s => { _account = s; return string.Empty; },
+            e => _errorMessage = e.First().ToString());
+    }
+
+    private void DeleteHandler()
+    {
+        var result = _repo.Delete(Id);
+        result.Match(
+            s => { _nav.NavigateTo("/account"); return string.Empty; },
+            e => _errorMessage = e.First().ToString());
+    }
+
+    private void CancelHandler() => _nav.NavigateTo("/account");
+}
