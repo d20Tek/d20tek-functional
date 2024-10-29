931c6c5358361df7dcecc6652396653c6ff15054
Pedro Silva <DarthPedro@gmail.com>
2024-10-29 12:08:26 -0700

    Refactored the main program startup code. Added WealthTracker.Domain namespace to _Imports.razor.

--- samples/Blazor/WealthTracker/Program.cs
+++ samples/Blazor/WealthTracker/Program.cs
@@ -5,11 +5,15 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WealthTracker;
 using WealthTracker.Persistence;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+await CreateBlazorApplication(args)
+        .Map(builder => builder.Build())
+        .Map(app => app.RunAsync()).Get();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddRepository();
-
-await builder.Build().RunAsync();
+Identity<WebAssemblyHostBuilder> CreateBlazorApplication(string[] args) =>
+    WebAssemblyHostBuilder.CreateDefault(args)
+                          .ToIdentity()
+                          .Iter(b => b.RootComponents.Add<App>("#app"))
+                          .Iter(b => b.RootComponents.Add<HeadOutlet>("head::after"))
+                          .Iter(b => b.Services.AddScoped(sp =>
+                                new HttpClient { BaseAddress = new Uri(b.HostEnvironment.BaseAddress) })
+                                .AddRepository());
