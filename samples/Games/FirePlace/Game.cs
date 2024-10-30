72de6d011f03b228a5232e915de98838c35d894f
Pedro Silva <DarthPedro@gmail.com>
2024-10-30 16:31:27 -0700

    Finished functionalizing the Game class.

--- samples/Games/FirePlace/Game.cs
+++ samples/Games/FirePlace/Game.cs
@@ -9,18 +9,17 @@ internal static class Game
     public delegate int RndFunc(int min, int max);
 
     public static void Play(IAnsiConsole console, RndFunc rnd) =>
-        Initialize(console, rnd)
-            .Map(initialFrame =>
-                initialFrame.IterateUntil(
-                    x => x.NextFrame(console, rnd),
-                    x => x.GameRunning is false))
-            .Iter(x => console.WriteMessage(Constants.EndMessage));
+        console.Initialize(rnd)
+               .Map(initialFrame =>
+                    initialFrame.IterateUntil(
+                        x => x.NextFrame(console, rnd),
+                        x => x.GameRunning is false))
+               .Iter(x => console.WriteMessage(Constants.EndMessage));
 
-    private static GameState Initialize(IAnsiConsole console, RndFunc rnd)
-    {
-        console.ShowStartMessage();
-        return new(FireMatrix.Initialize(rnd));
-    }
+    private static GameState Initialize(this IAnsiConsole console, RndFunc rnd) =>
+        console.ToIdentity()
+               .Iter(c => c.ShowStartMessage())
+               .Map(_ => new GameState (FireMatrix.Initialize(rnd)));
 
     private static void ShowStartMessage(this IAnsiConsole console) =>
         console.ToIdentity()
@@ -30,17 +29,14 @@ internal static class Game
 
     private static GameState NextFrame(this GameState state, IAnsiConsole console, RndFunc rnd) =>
         KeyboardInput.Handle(state, console)
-            .Map(s => PropagateFire(s, rnd))
-            .Iter(s => RenderFire(s, console))
+            .Map(s => UpdateFire(s, rnd))
+            .Iter(s => console.RenderFire(s))
             .Iter(s => Thread.Sleep(Constants.RefreshRate));
 
-    private static GameState PropagateFire(GameState state, RndFunc rnd)
-    {
-        var matrix = state.FireMatrix.PropagateFire(rnd);
-        return state with { FireMatrix = matrix };
-    }
+    private static GameState UpdateFire(GameState state, RndFunc rnd) =>
+        state with { FireMatrix = state.FireMatrix.PropagateFire(rnd) };
 
-    static void RenderFire(GameState state, IAnsiConsole console)
+    static void RenderFire(this IAnsiConsole console, GameState state)
     {
         Console.SetCursorPosition(0, 0);
 
