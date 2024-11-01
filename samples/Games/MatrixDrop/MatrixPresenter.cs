8456207320d42b16b5091123d26522e0a43a8391
Pedro Silva <DarthPedro@gmail.com>
2024-10-31 23:43:07 -0700

    Refactored MatrixPresenter to be more functional in style.

--- samples/Games/MatrixDrop/MatrixPresenter.cs
+++ samples/Games/MatrixDrop/MatrixPresenter.cs
@@ -4,15 +4,8 @@ namespace MatrixDrop;
 
 internal static class MatrixPresenter
 {
-    public static List<string> Render(this IAnsiConsole console, List<List<MatrixCell>> matrix, int height)
-    {
-        var outputLines = new List<string>();
-
-        for (int y = 0; y < height; y++)
-        {
-            outputLines.Add(string.Join(" ", matrix[y]));
-        }
-
-        return outputLines;
-    }
+    public static List<string> Render(this IAnsiConsole console, List<List<MatrixCell>> matrix, int height) =>
+        matrix.Take(height)
+              .Select(row => string.Join(" ", row))
+              .ToList();
 }
