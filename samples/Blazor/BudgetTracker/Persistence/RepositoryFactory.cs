daef785144771ea8ad6a3887203f303813000dec
Pedro Silva <DarthPedro@gmail.com>
2024-11-03 21:46:07 -0800

    Added BudgetDataStore data object for persistence. Added repository registration for dependency injection.

---
+++ samples/Blazor/BudgetTracker/Persistence/RepositoryFactory.cs
@@ -0,0 +1,22 @@
+﻿using Apps.Repositories;
+using BudgetTracker.Domain;
+using D20Tek.LowDb;
+using D20Tek.LowDb.Browser;
+
+namespace BudgetTracker.Persistence;
+
+internal static class RepositoryFactory
+{
+    public const string _databaseKey = "wealth-data-key";
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services) =>
+        services.AddLocalLowDb<BudgetDataStore>(_databaseKey)
+                .AddScoped<ICategoryRepository, FileRepository<BudgetCategory, BudgetDataStore>>(sp =>
+                    new(sp.GetRequiredService<LowDb<BudgetDataStore>>(), store => store.Categories))
+                .AddScoped<IIncomeRepository, FileRepository<Income, BudgetDataStore>>(sp =>
+                    new(sp.GetRequiredService<LowDb<BudgetDataStore>>(), store => store.Incomes))
+                .AddScoped<IExpenseRepository, FileRepository<Expense, BudgetDataStore>>(sp =>
+                    new (sp.GetRequiredService<LowDb<BudgetDataStore>>(), store => store.Expenses))
+                .AddScoped<IReconciledSnapshotRepository, FileRepository<ReconciledSnapshot, BudgetDataStore>>(sp =>
+                    new (sp.GetRequiredService<LowDb<BudgetDataStore>>(), store => store.CompletedSnapshots));
+}
--- samples/Blazor/BudgetTracker/Program.cs
+++ samples/Blazor/BudgetTracker/Program.cs
@@ -1,4 +1,5 @@
 using BudgetTracker;
+using BudgetTracker.Persistence;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+                .AddRepositories();
 
 await builder.Build().RunAsync();
