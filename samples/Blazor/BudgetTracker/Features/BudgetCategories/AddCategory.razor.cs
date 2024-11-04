a6d6102dbfb2ec4c9c3bea4ae11260d04d347d7a
Pedro Silva <DarthPedro@gmail.com>
2024-11-04 14:07:08 -0800

    Created the AddCategory page and integration with ICategoryRepository. Added common ResultExtensions class for handling Result<T> in Blazor app.

---
+++ samples/Blazor/BudgetTracker/Features/BudgetCategories/AddCategory.razor.cs
@@ -0,0 +1,23 @@
+﻿using BudgetTracker.Common;
+using BudgetTracker.Domain;
+
+namespace BudgetTracker.Features.BudgetCategories;
+
+public partial class AddCategory
+{
+    public class ViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public decimal BudgetedAmount { get; set; }
+    }
+
+    private string _errorMessage = string.Empty;
+    private readonly ViewModel _vm = new();
+
+    private void CreateCategory() =>
+        _repo.Create(new BudgetCategory(0, _vm.Name, _vm.BudgetedAmount))
+             .HandleResult(s => _nav.NavigateTo(Constants.Categories.ListUrl), e => _errorMessage = e);
+
+    private void CancelHandler() => _nav.NavigateTo(Constants.Categories.ListUrl);
+}
