fcf7686b138728d11e5aae0e8e8ad11dceb9c8b6
Pedro Silva <DarthPedro@gmail.com>
2024-11-03 22:22:28 -0800

    Added categories list page. Linked nav menu to list page.

--- samples/Blazor/BudgetTracker/Constants.cs
+++ samples/Blazor/BudgetTracker/Constants.cs
@@ -4,4 +4,12 @@ internal static class Constants
 {
     public const string TotalIncomeLabel = "Total Income:";
     public const string TotalExpensesLabel = "Total Expenses:";
+
+    public static class Categories
+    {
+        public const string ListUrl = "/budget-category";
+        public const string AddUrl = "/budget-category/add";
+        public static string EditUrl(int id) => $"/budget-category/edit/{id}";
+        public static string DeleteUrl(int id) => $"/budget-category/delete/{id}";
+    }
 }
