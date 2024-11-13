2c2ac649c7464f629b5127793ef1a40fdd601504
Pedro Silva <DarthPedro@gmail.com>
2024-11-12 23:06:31 -0800

    Added initial reconcile month page.

---
+++ samples/Blazor/BudgetTracker/Features/Snapshots/ReconcileMonth.razor.cs
@@ -0,0 +1,16 @@
+﻿using BudgetTracker.Domain;
+
+namespace BudgetTracker.Features.Snapshots;
+
+public partial class ReconcileMonth
+{
+    internal class ViewModel
+    {
+        public DateTimeOffset ReconcileDate { get; set; } = DateTimeOffset.Now;
+    }
+
+    private string _errorMessage = string.Empty;
+    private readonly ViewModel _vm = new();
+
+    private void ReconcileHandler() { }
+}
