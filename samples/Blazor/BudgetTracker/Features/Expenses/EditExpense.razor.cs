724ed6d0477d06b76dfdb68a2a84be1a7b6a9c00
Pedro Silva <DarthPedro@gmail.com>
2024-11-12 22:31:40 -0800

    Implemented edit expense page and repository integration. Fixed bug in category id mapping in list expenses page.

---
+++ samples/Blazor/BudgetTracker/Features/Expenses/EditExpense.razor.cs
@@ -0,0 +1,52 @@
+﻿using BudgetTracker.Common;
+using BudgetTracker.Domain;
+using D20Tek.Functional;
+using Microsoft.AspNetCore.Components;
+
+namespace BudgetTracker.Features.Expenses;
+
+public partial class EditExpense
+{
+    internal class ViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+
+        public BudgetCategory[] Categories { get; set; } = [];
+
+        public DateTimeOffset CommittedDate { get; set; } = DateTimeOffset.Now;
+
+        public decimal Actual { get; set; }
+    }
+
+    private string _errorMessage = string.Empty;
+    private Option<ViewModel> _vm = Option<ViewModel>.None();
+
+    [Parameter]
+    public int Id { get; set; }
+
+    protected override void OnInitialized() =>
+        _repo.GetEntityById(Id)
+             .HandleResult(
+                s => _vm = new ViewModel
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    CategoryId = s.CategoryId,
+                    Categories = _catRepo.GetEntities(),
+                    CommittedDate = s.CommittedDate,
+                    Actual = s.Actual
+                },
+                e => _errorMessage = e);
+
+    private void UpdateHandler() =>
+        _vm.MatchAction(
+            a => _repo.Update(new Expense(a.Id, a.Name, a.CategoryId, a.CommittedDate, a.Actual))
+                      .HandleResult(s => _nav.NavigateTo(Constants.Expense.ListUrl), e => _errorMessage = e),
+            () => _errorMessage = Constants.Income.MissingIncomeError);
+
+    private void CancelHandler() => _nav.NavigateTo(Constants.Expense.ListUrl);
+}
