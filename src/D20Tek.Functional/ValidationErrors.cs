946b4c2f16968c474a24509ef87f3ac316c19e44
Pedro Silva <DarthPedro@gmail.com>
2025-03-10 11:36:21 -0700

    Added ValidationErrors class to simplify WebApi validation checking.

---
+++ src/D20Tek.Functional/ValidationErrors.cs
@@ -0,0 +1,25 @@
+﻿namespace D20Tek.Functional;
+
+public class ValidationErrors
+{
+    private readonly IList<Error> _errors = [];
+
+    public bool HasErrors => _errors.Count > 0;
+
+    private ValidationErrors() { }
+
+    public static ValidationErrors Create() => new();
+
+    public ValidationErrors AddIfError(Func<bool> check, Error error)
+    {
+        if (check()) _errors.Add(error);
+        return this;
+    }
+
+    public ValidationErrors AddIfError(Func<bool> check, string code, string message) =>
+        AddIfError(check, Error.Validation(code, message));
+
+    public Result<T> Map<T>(Func<T> onSuccess) where T : notnull => HasErrors ? ToFailure<T>() : onSuccess();
+
+    public Result<T> ToFailure<T>() where T : notnull => Result<T>.Failure([.. _errors]);
+}
---
+++ tests/D20Tek.Functional.UnitTests/ValidationErrorsTests.cs
@@ -0,0 +1,110 @@
+﻿namespace D20Tek.Functional.UnitTests;
+
+[TestClass]
+public class ValidationErrorsTests
+{
+    [TestMethod]
+    public void AddIfError_WithError_AddsToList()
+    {
+        // arrange
+        var errors = ValidationErrors.Create();
+
+        // act
+        errors.AddIfError(() => true, "Test.Error", "Test message");
+
+        // assert
+        errors.HasErrors.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void AddIfError_WithNoError_KeepsListEmpty()
+    {
+        // arrange
+        var errors = ValidationErrors.Create();
+
+        // act
+        errors.AddIfError(() => false, "Test.Error", "Test message");
+
+        // assert
+        errors.HasErrors.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void AddIfError_WithErrorType_AddsToList()
+    {
+        // arrange
+        var err = Error.Validation("Test.Error", "Test message");
+        var errors = ValidationErrors.Create();
+
+        // act
+        errors.AddIfError(() => true, err);
+
+        // assert
+        errors.HasErrors.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void AddIfError_WithNoErrorType_KeepsListEmpty()
+    {
+        // arrange
+        var err = Error.Validation("Test.Error", "Test message");
+        var errors = ValidationErrors.Create();
+
+        // act
+        errors.AddIfError(() => false, err);
+
+        // assert
+        errors.HasErrors.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void ToFailure_WithError_ReturnsFailureResult()
+    {
+        // arrange
+        var err = Error.Validation("Test.Error", "Test message");
+        var errors = ValidationErrors.Create();
+        errors.AddIfError(() => true, err);
+
+        // act
+        var result = errors.ToFailure<Result<bool>>();
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsFailure.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Map_WithError_ReturnsFailureResult()
+    {
+        // arrange
+        var err = Error.Validation("Test.Error", "Test message");
+        var errors = ValidationErrors.Create();
+        errors.AddIfError(() => true, err);
+
+        // act
+        var result = errors.Map(() => false);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsFailure.Should().BeTrue();
+        result.IsSuccess.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Map_WithoutError_CallsOnSuccess()
+    {
+        // arrange
+        var err = Error.Validation("Test.Error", "Test message");
+        var errors = ValidationErrors.Create();
+        errors.AddIfError(() => false, err);
+
+        // act
+        var result = errors.Map(() => true);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsFailure.Should().BeFalse();
+        result.IsSuccess.Should().BeTrue();
+        result.GetValue().Should().BeTrue();
+    }
+}
