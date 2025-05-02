fa65e8c95600dce625cde285b710f8991072938c
Pedro Silva <DarthPedro@gmail.com>
2025-05-02 15:42:24 -0700

    Added async methods for functional BooleanExtensions.

---
+++ src/D20Tek.Functional/Async/BoolAsyncExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace D20Tek.Functional.Async;
+
+public static class BoolAsyncExtensions
+{
+    public static async Task<TOut> IfTrueOrElseAsync<TOut>(
+        this bool condition,
+        Func<Task<TOut>> thenFunc,
+        Func<Task<TOut>> elseFunc) =>
+        condition ? await thenFunc() : await elseFunc();
+
+    public static async Task IfTrueOrElseAsync(
+        this bool condition,
+        Func<Task> thenAction,
+        Func<Task>? elseAction = null)
+    {
+        if (condition)
+            await thenAction();
+        else if (elseAction is not null)
+            await elseAction.Invoke();
+    }
+
+    public static async Task<TOut> IfTrueOrElseAsync<TOut>(
+        this Task<bool> condition,
+        Func<Task<TOut>> thenFunc,
+        Func<Task<TOut>> elseFunc) =>
+        await(await condition).IfTrueOrElseAsync(thenFunc, elseFunc);
+
+    public static async Task IfTrueOrElseAsync(
+    this Task<bool> condition,
+    Func<Task> thenAction,
+    Func<Task>? elseAction = null) =>
+        await (await condition).IfTrueOrElseAsync(thenAction, elseAction);
+}
---
+++ tests/D20Tek.Functional.UnitTests/Async/BooleanExtensionsTests.cs
@@ -0,0 +1,148 @@
+﻿using D20Tek.Functional.Async;
+
+namespace D20Tek.Functional.UnitTests.Async;
+
+[TestClass]
+public class BooleanAsyncExtensionsTests
+{
+    [TestMethod]
+    public async Task IfTrueOrElseAsyncFunc_WithPositiveCondition_ReturnsThenFunc()
+    {
+        // arrange
+        var x = 42;
+
+        // act
+        var result = await (x > 5).IfTrueOrElseAsync(
+            () => Task.FromResult("thenFunc"),
+            [ExcludeFromCodeCoverage] () => Task.FromResult("elseFunc"));
+
+        // assert
+        result.Should().Be("thenFunc");
+    }
+
+    [TestMethod]
+    public async Task IfTrueOrElseAsyncFunc_WithNegativeCondition_ReturnsElseFunc()
+    {
+        // arrange
+        var x = 4;
+
+        // act
+        var result = await (x > 5).IfTrueOrElseAsync(
+            [ExcludeFromCodeCoverage] () => Task.FromResult("thenFunc"),
+            () => Task.FromResult("elseFunc"));
+
+        // assert
+        result.Should().Be("elseFunc");
+    }
+
+    [TestMethod]
+    public async Task IfTrueOrElseAsyncAction_WithPositiveCondition_CallsThenAction()
+    {
+        // arrange
+        var x = 42;
+        var result = 0;
+        Func<Task> thenAction = () => Task.FromResult(result += 1);
+        Func<Task> elseAction = [ExcludeFromCodeCoverage] () => Task.FromResult(result -= 1);
+
+        // act
+        await(x > 5).IfTrueOrElseAsync(thenAction, elseAction);
+
+        // assert
+        result.Should().Be(1);
+    }
+
+    [TestMethod]
+    public async Task IfTrueOrElseAsyncAction_WithPositiveConditionNoElse_CallsThenAction()
+    {
+        // arrange
+        var x = 42;
+        var result = 0;
+        Func<Task> thenAction = () => Task.FromResult(result += 1);
+
+        // act
+        await (x > 5).IfTrueOrElseAsync(thenAction);
+
+        // assert
+        result.Should().Be(1);
+    }
+
+    [TestMethod]
+    public async Task IfTrueOrElseAsyncAction_WithNegativeCondition_CallsElseAction()
+    {
+        // arrange
+        var x = 4;
+        var result = 0;
+        Func<Task> thenAction = [ExcludeFromCodeCoverage] () => Task.FromResult(result += 1);
+        Func<Task> elseAction = () => Task.FromResult(result -= 1);
+
+        // act
+        await (x > 5).IfTrueOrElseAsync(thenAction, elseAction);
+
+        // assert
+        result.Should().Be(-1);
+    }
+
+    [TestMethod]
+    public async Task IfTrueOrElseAsyncAction_WithNegativeConditionNoElse_MakesNoCall()
+    {
+        // arrange
+        var x = 4;
+        var result = 0;
+        Func<Task> thenAction = [ExcludeFromCodeCoverage] () => Task.FromResult(result += 1);
+
+        // act
+        await (x > 5).IfTrueOrElseAsync(thenAction);
+
+        // assert
+        result.Should().Be(0);
+    }
+
+    [TestMethod]
+    public async Task IfTrueOrElseAsyncFunc_WithAsyncAndPositiveCondition_ReturnsThenFunc()
+    {
+        // arrange
+        var x = 42;
+        Task<bool> task = Task.FromResult(x > 5);
+
+        // act
+        var result = await task.IfTrueOrElseAsync(
+            () => Task.FromResult("thenFunc"),
+            [ExcludeFromCodeCoverage] () => Task.FromResult("elseFunc"));
+
+        // assert
+        result.Should().Be("thenFunc");
+    }
+
+    [TestMethod]
+    public async Task IfTrueOrElseAsyncFunc_WithAsyncAndNegativeCondition_ReturnsElseFunc()
+    {
+        // arrange
+        var x = 4;
+        Task<bool> task = Task.FromResult(x > 5);
+
+        // act
+        var result = await task.IfTrueOrElseAsync(
+            [ExcludeFromCodeCoverage] () => Task.FromResult("thenFunc"),
+            () => Task.FromResult("elseFunc"));
+
+        // assert
+        result.Should().Be("elseFunc");
+    }
+
+    [TestMethod]
+    public async Task IfTrueOrElseAsyncAction_WithAsyncAndPositiveCondition_CallsThenAction()
+    {
+        // arrange
+        var x = 42;
+        var task = Task.FromResult(x > 5);
+        var result = 0;
+        Func<Task> thenAction = () => Task.FromResult(result += 1);
+        Func<Task> elseAction = [ExcludeFromCodeCoverage] () => Task.FromResult(result -= 1);
+
+        // act
+        await task.IfTrueOrElseAsync(thenAction, elseAction);
+
+        // assert
+        result.Should().Be(1);
+    }
+}
