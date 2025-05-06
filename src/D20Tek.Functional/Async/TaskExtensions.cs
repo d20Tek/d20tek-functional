5270ee53190b59b9e72480151c0ec810853b90e9
Pedro Silva <DarthPedro@gmail.com>
2025-05-06 16:28:30 -0700

    Added functional extension to async Task type to chain multiple tasks together using ThenAsync.

---
+++ src/D20Tek.Functional/Async/TaskExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace D20Tek.Functional.Async;
+
+public static class TaskExtensions
+{
+    public static async Task<TOut> ThenAsync<TIn, TOut>(this Task<TIn> task, Func<TIn, Task<TOut>> func) =>
+        await func(await task);
+}
---
+++ tests/D20Tek.Functional.UnitTests/Async/TaskExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using D20Tek.Functional.Async;
+
+namespace D20Tek.Functional.UnitTests.Async;
+
+[TestClass]
+public class TaskExtensionsTests
+{
+    [TestMethod]
+    public async Task ThenAsync_WithSameOutType_ReturnsTask()
+    {
+        // arrange
+        var task = Task.FromResult(10);
+
+        // act
+        var resultingTask = task.ThenAsync(x => Task.FromResult(x + 5));
+        var result = await resultingTask;
+
+        // assert
+        resultingTask.Should().BeAssignableTo<Task<int>>();
+        result.Should().Be(15);
+    }
+
+    [TestMethod]
+    public async Task ThenAsync_WithDiffOutType_ReturnsTaskTOut()
+    {
+        // arrange
+        var task = Task.FromResult(10);
+
+        // act
+        var resultingTask = task.ThenAsync(x => Task.FromResult(x.ToString()));
+        var result = await resultingTask;
+
+        // assert
+        resultingTask.Should().BeAssignableTo<Task<string>>();
+        result.Should().Be("10");
+    }
+}
