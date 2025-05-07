71d0b68c9e6b612751f679e6cc5bbb79c9ae5d8d
Pedro Silva <DarthPedro@gmail.com>
2025-05-07 13:36:15 -0700

    Added async extension functions to Controller based WebApi result methods.

---
+++ src/D20Tek.Functional.AspNetCore/WebApi/ResultAsyncExtensions.cs
@@ -0,0 +1,63 @@
+﻿using D20Tek.Functional.Async;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D20Tek.Functional.AspNetCore.WebApi.Async;
+
+public static class ResultAsyncExtensions
+{
+    public static async Task<ActionResult<TResponse>> ToActionResultAsync<TValue, TResponse>(
+        this Task<Result<TValue>> result,
+        Func<TValue, TResponse> responseMap,
+        ControllerBase controller) where TValue : notnull =>
+        await result.MatchAsync(
+            s => Task.FromResult<ActionResult<TResponse>>(controller.Ok(responseMap(s))),
+            e => Task.FromResult(controller.Problem<TResponse>(e)));
+
+    public static async Task<ActionResult<TResponse>> ToActionResultAsync<TValue, TResponse>(
+        this Task<Result<TValue>> result,
+        TResponse response,
+        ControllerBase controller) where TValue : notnull =>
+        await result.MatchAsync(
+            s => Task.FromResult<ActionResult<TResponse>>(controller.Ok(response)), 
+            e => Task.FromResult(controller.Problem<TResponse>(e)));
+
+    public static async Task<ActionResult<TResponse>> ToCreatedActionResultAsync<TValue, TResponse>(
+        this Task<Result<TValue>> result,
+        Func<TValue, TResponse> responseMap,
+        ControllerBase controller,
+        string? routeName = null,
+        object? routeValues = null) where TValue : notnull =>
+        await result.MatchAsync(
+            s => Task.FromResult<ActionResult<TResponse>>(
+                        controller.CreatedAtAction(routeName, routeValues, responseMap(s))),
+            e => Task.FromResult(controller.Problem<TResponse>(e)));
+
+    public static async Task<ActionResult<TResponse>> ToCreatedActionResultAsync<TValue, TResponse>(
+        this Task<Result<TValue>> result,
+        TResponse response,
+        ControllerBase controller,
+        string? routeName = null,
+        object? routeValues = null) where TValue : notnull =>
+        await result.MatchAsync(
+            s => Task.FromResult<ActionResult<TResponse>>(
+                        controller.CreatedAtAction(routeName, routeValues, response)),
+            e => Task.FromResult(controller.Problem<TResponse>(e)));
+
+    public static async Task<ActionResult<TResponse>> ToCreatedActionResultAsync<TValue, TResponse>(
+        this Task<Result<TValue>> result,
+        Func<TValue, TResponse> responseMap,
+        ControllerBase controller,
+        string routeUri) where TValue : notnull =>
+        await result.MatchAsync(
+            s => Task.FromResult<ActionResult<TResponse>>(controller.Created(routeUri, responseMap(s))),
+            e => Task.FromResult(controller.Problem<TResponse>(e)));
+
+    public static async Task<ActionResult<TResponse>> ToCreatedActionResultAsync<TValue, TResponse>(
+        this Task<Result<TValue>> result,
+        TResponse response,
+        ControllerBase controller,
+        string routeUri) where TValue : notnull =>
+        await result.MatchAsync(
+            s => Task.FromResult<ActionResult<TResponse>>(controller.Created(routeUri, response)),
+            e => Task.FromResult(controller.Problem<TResponse>(e)));
+}
